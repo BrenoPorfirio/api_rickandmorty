c33120db6f57c199e10ca583ee9c54798d3352a8
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-06-01 14:20:53 -0300

    Organizando o projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-class DataService{
-  final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
-  void carregar(index){
-    final funcoes = [];
-    funcoes[index]();
-  }
-
-  // void (){
-  //   return;
-  // }
-
-  // void (){
-  //   return;
-  // }
-
-  Future<void> () async{
-    var beersUri = Uri(
-      scheme: 'https',
-      host: '',
-      path: '',
-      queryParameters: {'size': '1'});
-    var jsonString = await http.read(beersUri);
-    var beersJson = jsonDecode(jsonString);
-    tableStateNotifier.value = beersJson;
-  }
-}
-
-final dataService = DataService();
+import 'package:api_rickandmorty/home/screens/ui/home.dart';
 
 void main() {
-  MyApp app = MyApp();
-  runApp(app);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-      debugShowCheckedModeBanner:false,
-      home: Scaffold(
-        appBar: AppBar( 
-          title: const Text("Rick and Morty Characters"),
-          ),
-        body: ValueListenableBuilder(
-          valueListenable: dataService.tableStateNotifier,
-          builder:(_, value, __){
-            return DataTableWidget(
-              jsonObjects:value, 
-              // propertyNames: ["name","status","species"], 
-              // columnNames: ["Nome", "Status", "Espécie"]
-            );
-          }
-        ),
-        bottomNavigationBar: NewNavBar(itemSelectedCallback: dataService.carregar),
-      ));
+      title: "API RICK AND MORTY",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
   }
 }
 
-class NewNavBar extends HookWidget {
-  final _itemSelectedCallback;
-  NewNavBar({itemSelectedCallback}):
-    _itemSelectedCallback = itemSelectedCallback ?? (int){}
-  @override
-  Widget build(BuildContext context) {
-    var state = useState(1);
-    return BottomNavigationBar(
-      onTap: (index){
-        state.value = index;
-        _itemSelectedCallback(index);                
-      }, 
-      currentIndex: state.value,
-      items: const [
-        BottomNavigationBarItem(
-          label: "Ricks",
-          icon: Icon(Icons.person),
-        ),
-        BottomNavigationBarItem(
-            label: "Mortys", icon: Icon(Icons.location_city)),
-        BottomNavigationBarItem(
-          label: "Random persons", icon: Icon(Icons.shape_line))
-      ]);
-  }
-}
+// class NewNavBar extends HookWidget {
+//   final _itemSelectedCallback;
+//   NewNavBar({itemSelectedCallback})
+//       : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
+//   @override
+//   Widget build(BuildContext context) {
+//     var state = useState(1);
+//     return BottomNavigationBar(
+//         onTap: (index) {
+//           state.value = index;
+//           _itemSelectedCallback(index);
+//         },
+//         currentIndex: state.value,
+//         items: const [
+//           BottomNavigationBarItem(
+//             label: "Ricks",
+//             icon: Icon(Icons.person),
+//           ),
+//           BottomNavigationBarItem(
+//               label: "Mortys", icon: Icon(Icons.location_city)),
+//           BottomNavigationBarItem(
+//               label: "Random persons", icon: Icon(Icons.shape_line))
+//         ]);
+//   }
+// }
 
-class DataTableWidget extends StatelessWidget {
-  final List jsonObjects;
-  final List<String> columnNames;
-  final List<String> propertyNames;
-
-  DataTableWidget( {this.jsonObjects = const [], this.columnNames = const ["","",""], this.propertyNames= const ["", "", ""]});
-
-  @override
-  Widget build(BuildContext context) {
-    return DataTable(
-      columns: columnNames.map( 
-                (name) => DataColumn(
-                  label: Expanded(
-                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
-                  )
-                )
-              ).toList()       
-      ,
-      rows: jsonObjects.map( 
-        (obj) => DataRow(
-            cells: propertyNames.map(
-              (propName) => DataCell(Text(obj[propName]))
-            ).toList()
-          )
-        ).toList());
-  }
-}
+// essa parte do código vai para home.
 // inicio da pagina de login:
 
 //       body: Stack(
